2e1bb1a04df1c58026a5d0ba415d1a680efef619
Shream9 <97399240+shream1@users.noreply.github.com>
2025-03-17 11:07:32 -0800

    Main

---
+++ Assets/shream/Code/PlayerSpawner.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSpawner : MonoBehaviour
+{
+
+    public GameObject[] playerPrefabs;
+    public Transform[] spawnNodes;
+
+
+    public void Start()
+    {
+        int randomNumber = Random.Range(0, spawnNodes.Length);
+        Transform spawnNode = spawnNodes[randomNumber];
+        GameObject playerToSpawn = playerPrefabs[(int)PhotonNetwork.LocalPlayer.CustomProperties["playerAvatar"]];
+        PhotonNetwork.Instantiate(playerToSpawn.name, spawnNode.position,Quaternion.identity);
+    }
+
+
+}

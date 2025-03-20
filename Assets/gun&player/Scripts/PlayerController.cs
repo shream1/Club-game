51737068e6f5f51d87a60f50805fb3b37d29f0f6
Shream9 <97399240+shream1@users.noreply.github.com>
2025-03-20 10:55:56 -0800

    MainCharter

--- Assets/gun&player/Scripts/PlayerController.cs
+++ Assets/gun&player/Scripts/PlayerController.cs
@@ -1,22 +1,32 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPun
 {
     private Rigidbody2D rb;
     [SerializeField] private float speed = 5f;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        if (!photonView.IsMine)
+        {
+            Destroy(GetComponent<PlayerController>());
+        }
+        else
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
     }
 
     private void Update()
     {
+        if (!photonView.IsMine) return;
         Move();
     }
 
     private void Move()
     {
+        if (!photonView.IsMine) return;
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
